02b4d3e14613546ab6ad84e27536462e0a231690
garima shrestha <garimashrestha20@gmail.com>
2024-12-13 10:54:56 +0545

    Button Navigation Completed

---
+++ app/src/main/java/com/example/task/ButtonActivity.kt
@@ -0,0 +1,47 @@
+package com.example.task
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.task.databinding.ActivityButtonBinding
+
+class ButtonActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityButtonBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        binding = ActivityButtonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnNavigate.setOnClickListener {
+
+            val user: String = binding.username.text.toString()
+            val pass: String = binding.password.text.toString()
+
+            if (user.isEmpty()) {
+                binding.password.error = "User can't be empty"
+            } else if (pass.isEmpty()) {
+                binding.password.error = "Password can't be empty"
+            } else {
+                val intent = Intent(this@ButtonActivity, DestinationActivity::class.java)
+                intent.putExtra("username", user)
+                intent.putExtra("password", pass)
+                startActivity(intent)
+                finish()    //if yo garyo bhnaye tyo activity back jadaina
+            }
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
