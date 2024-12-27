90b528b105112783b2ecf48ce065375bf6a2c58d
garima shrestha <garimashrestha20@gmail.com>
2024-12-27 10:49:29 +0545

    Order Acitivity

---
+++ app/src/main/java/com/example/task/adapter/TabAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.task.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.task.UI.fragment.AcitveFragment
+import com.example.task.UI.fragment.CancelledFragment
+import com.example.task.UI.fragment.DeliveredFragment
+
+class TabAdapter (
+    fragmentManager : FragmentManager, lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager,lifecycle) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0 -> return AcitveFragment()
+            1 -> return CancelledFragment()
+            2 -> return DeliveredFragment()
+            else -> return AcitveFragment()
+        }
+    }
+}
